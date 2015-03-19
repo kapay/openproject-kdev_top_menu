ef3e5a45d5c6b52622c477925e1b32d88084376e
kapay <kapay@users.noreply.github.com>
2015-03-18 18:08:44 -0700

    Update engine.rb

--- lib/open_project/kdev_top_menu/engine.rb
+++ lib/open_project/kdev_top_menu/engine.rb
@@ -13,8 +13,8 @@ module OpenProject::KdevTopMenu
              :requires_openproject => '>= 4.0.0' do
 
       # menu :real_top_menu, :customizations, {}, :caption => "Customizations", :children => Proc.new {[Redmine::MenuManager::MenuItem.new(:custom_child, 'http://example.com', :caption => "custom child")]}
-      menu :real_top_menu, :customization_projects, {:controller => 'projects', :action => 'requirements/wiki/Customization_projects'}, :caption => "Customization projects", :if => Proc.new {User.current.logged?}
-      menu :real_top_menu, :requirements, {}, :caption => "Requirements", :if => Proc.new {User.current.logged?}
+      menu :real_top_menu, :customization_projects, {:controller => 'projects', :action => 'requirements/wiki/Customization_projects'}, :caption => "Customization projects"
+      menu :real_top_menu, :requirements, {}, :caption => "Requirements"
       menu :real_top_menu, :customization_req, {:controller => 'projects', :action => 'requirements/wiki/Customizations'}, :caption => "Customizations", :parent => :requirements
       menu :real_top_menu, :product_req, {:controller => 'projects', :action => 'requirements/wiki/Products'}, :caption => "Products", :parent => :requirements
       # menu :real_top_menu, :example_art_link2, {:controller => 'projects', :action => 'requirements/wiki/Customizations'}, :caption => "Product Requirements", :parent => :requirements
